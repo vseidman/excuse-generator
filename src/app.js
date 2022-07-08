e9e217d59046fee578983b1c1230f5087d28962d
vseidman <victorseidman@gmail.com>
2022-07-08 21:12:40 +0000

    Generador de excusa listo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#excusa").innerHTML = generateExcuse();
+};
+
+let generateExcuse = () => {
+  let person = ["Mi", "El"];
+  let subject = ["perro", "iguana", "sapo", "ventilador"];
+  let action = ["se comio", "mojo", "destrozo", "ensucio"];
+  let object = ["tarea", "franela", "zapato", "telefono"];
+  let where = ["en mi casa", "en el carro", "en el colegio"];
+
+  let perInx = Math.floor(Math.random() * person.length);
+  let subInx = Math.floor(Math.random() * subject.length);
+  let acInx = Math.floor(Math.random() * action.length);
+  let obInx = Math.floor(Math.random() * object.length);
+  let whInx = Math.floor(Math.random() * where.length);
+
+  return (
+    person[perInx] +
+    " " +
+    subject[subInx] +
+    " " +
+    action[acInx] +
+    " " +
+    object[obInx] +
+    " " +
+    where[whInx]
+  );
 };
